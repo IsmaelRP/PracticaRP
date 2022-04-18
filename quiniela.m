9de1ba91b2ebd18330bd40d687c258ce03371ced
Ismael <ismael98r@hotmail.com>
2022-04-18 17:41:50 +0200

    Regresion GLM
    Primera versión de un ajuste GLM acorde a las victorias de los equipos que se enfrentan

--- quiniela.m
+++ quiniela.m
@@ -2,11 +2,11 @@ function y = quiniela(x)
     
     y = [];
 
-    %   NO TOCAR ESTE ARRAY DE STRING BAJO NINGÚN CONCEPTO
+    %   Equipos de 1º y 2º división según sus índices
     equipos = ["Alaves" "Ath Bilbao" "Ath Madrid" "Barcelona" "Betis" "Cadiz" "Celta" "Elche" "Espanol" "Getafe" "Granada" "Levante" "Mallorca" "Osasuna" "Real Madrid" "Sevilla" "Sociedad" "Vallecano" "Villarreal" "Alcorcon" "Almeria" "Amorebieta" "Burgos" "Cartagena" "Eibar" "Fuenlabrada" "Girona" "Huesca" "Ibiza" "Las Palmas" "Leganes" "Lugo" "Malaga" "Mirandes" "Oviedo" "Ponferradina" "Sociedad B" "Sp Gijon" "Tenerife" "Valladolid" "Zaragoza"];
 
     %   Para simular el input
-    x = [1 13 6 19 4 12 17 9 21 29 27 32 23 33 11; 14 3 15 2 20 5 8 16 35 25 31 22 28 26 7];
+    %   x = [1 13 6 19 4 12 17 9 21 29 27 32 23 33 11; 14 3 15 2 20 5 8 16 35 25 31 22 28 26 7];
 
     data1 = readtable('./SP1.csv');     %   Cargar la primera división
     data2 = readtable('./SP2.csv');     %   Cargar la segunda división
